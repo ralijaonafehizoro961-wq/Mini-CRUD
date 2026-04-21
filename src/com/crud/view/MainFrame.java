32fae28839393dc29c4f36aab7fb82ed583150b1
ralijaonafehizoro961-wq <ralijaonafehizoro961@gmail.com>
2026-04-21 14:51:50 +0300

    variable initialization from MainFrame

--- src/com/crud/view/MainFrame.java
+++ src/com/crud/view/MainFrame.java
@@ -1,5 +1,18 @@
 package com.crud.view;
 
-public class MainFrame {
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+import javax.swing.*;
+
+import com.crud.config.ConnectionDB;
+
+public class MainFrame extends JFrame{
+	Statement st;
+    ConnectionDB con = new ConnectionDB();
+    ResultSet rst;
+
+    JTextField id, nom;
+    JLabel txtid, txtnom;
+    JButton ajout, modifier, supprimer;
 }
